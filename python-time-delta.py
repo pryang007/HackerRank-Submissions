a4a02faacf96d852d7a79b4ab3e7316bc24392a5
Pryanshu Gupta <pryanshug88@gmail.com>
2024-02-26 22:54:13 +0530

    Create python-time-delta.py

---
+++ python-time-delta.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the time_delta function below.
+def time_delta(t1, t2):
+    
+    from datetime import datetime
+    
+    t1, t2 = map(lambda s:datetime.strptime(s, "%a %d %b %Y %H:%M:%S %z"), [t1, t2])
+    
+    return str(int(abs(t1-t2).total_seconds()))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        t1 = input()
+
+        t2 = input()
+
+        delta = time_delta(t1, t2)
+
+        fptr.write(delta + '\n')
+
+    fptr.close()
